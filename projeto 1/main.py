45c72615f5971226328d71087f9056c29f5ab389
ProHacker2020 <up202005321@up.pt>
2023-03-06 15:54:23 +0000

    commit

--- projeto 1/main.py
+++ projeto 1/main.py
@@ -19,7 +19,7 @@ BG = pygame.image.load("assets/Background.png")
 def render_multi_line(text, x, y, fsize):
         lines = text.splitlines()
         for i, l in enumerate(lines):
-            SCREEN.blit(get_font(30).render(l, 0, "White"), (x, y + fsize*2*i))
+            SCREEN.blit(get_font(20).render(l, 0, "White"), (x, y + fsize*2*i))
 
 def get_font(size): # Returns Press-Start-2P in the desired size
     return pygame.font.Font("assets/font.ttf", size)
@@ -29,8 +29,7 @@ def play():
     while True:
         SCREEN.blit(BG, (0, 0))
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
-        #BOARD = pygame.image.load("assets/board.png")
-        #SCREEN.blit(BOARD, (0, 0))
+    
         PLAY_TEXT = get_font(45).render("Game Mode:", True, "White")
         PLAY_RECT = PLAY_TEXT.get_rect(center=(300, 100))
         SCREEN.blit(PLAY_TEXT, PLAY_RECT)
@@ -79,11 +78,11 @@ def instructions():
         SCREEN.blit(BG, (0, 0))
         INSTRUCTIONS_MOUSE_POS = pygame.mouse.get_pos()
 
-        text = "Both players need four pieces to play.\nThe older player places their four pieces on four of the five forks around the outer ring.\nThe younger player places their four pieces on the central pentagon, each one on a fork opposite one of the older player’s pieces.\nThe older player takes the first turn. Thereafter players alternate.\nOn your turn, move one of your four pieces along a single edge to an empty adjacent fork.\nYou must move a piece.\nA piece is bound if is surrounded on all three sides, by any combination of players’ pieces.\nIf, at any time, one of your opponent’s pieces is bound, you have won the game."
+        text = "Both players need four pieces to play. The older player\nplaces their four pieces on four of the five forks around\nthe outer ring. The younger player places their four pieces\non the central pentagon, each one on a fork opposite one of\nthe older player’s pieces. The older player takes the first\nturn. Thereafter players alternate. On your turn, move one of\nyour four pieces along a single edge to an empty adjacent\nfork. You must move a piece. A piece is bound if is\nsurrounded on all three sides, by any combination of\nplayers’ pieces. If, at any time, one of your opponent’s\npieces is bound, you have won the game."
 
-        render_multi_line(text, 75, 200, 25)
+        render_multi_line(text, 50, 100, 20)
 
-        INSTRUCTIONS_BACK = Button(image=None, pos=(640, 460), 
+        INSTRUCTIONS_BACK = Button(image=None, pos=(640, 650), 
                             text_input="BACK", font=get_font(30), base_color="White", hovering_color="Red")
 
         INSTRUCTIONS_BACK.changeColor(INSTRUCTIONS_MOUSE_POS)
@@ -110,10 +109,10 @@ def main_menu():
 
         PLAY_BUTTON = Button(None, pos=(640, 375), 
                             text_input="PLAY", font=get_font(100), base_color="White", hovering_color="Red")
-        INSTRUCTIONS_BUTTON = Button(None, pos=(640, 450), 
+        INSTRUCTIONS_BUTTON = Button(None, pos=(640, 500), 
                             text_input="INSTRUCTIONS", font=get_font(30), base_color="White", hovering_color="Red")
-        QUIT_BUTTON = Button(None, pos=(640, 500), 
-                            text_input="QUIT", font=get_font(30), base_color="White", hovering_color="Red")
+        QUIT_BUTTON = Button(None, pos=(640, 600), 
+                            text_input="QUIT GAME", font=get_font(30), base_color="White", hovering_color="Red")
 
         SCREEN.blit(MENU_TEXT, MENU_RECT)
 
@@ -195,14 +194,14 @@ def pvp():
     while True:
         SCREEN.blit(BG, (0, 0))
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
-        #BOARD = pygame.image.load("assets/board.png")
-        #SCREEN.blit(BOARD, (0, 0))
-        PLAY_TEXT = get_font(45).render("PLAYER VS PLAYER", True, "White")
-        PLAY_RECT = PLAY_TEXT.get_rect(center=(400, 100))
+        BOARD = pygame.image.load("assets/board.png")
+        SCREEN.blit(BOARD, (340,100))
+        PLAY_TEXT = get_font(35).render("PLAYER VS PLAYER", True, "White")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 50))
         SCREEN.blit(PLAY_TEXT, PLAY_RECT)
 
-        PLAY_BACK = Button(image=None, pos=(640, 600), 
-                            text_input="BACK", font=get_font(50), base_color="White", hovering_color="Red")
+        PLAY_BACK = Button(image=None, pos=(175, 625), 
+                            text_input="QUIT GAME", font=get_font(35), base_color="White", hovering_color="Red")
 
         PLAY_BACK.changeColor(PLAY_MOUSE_POS)
         PLAY_BACK.update(SCREEN)
@@ -213,7 +212,7 @@ def pvp():
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
-                    play()
+                    main_menu()
 
         pygame.display.update()
 
@@ -221,14 +220,14 @@ def pvc(dif):
     while True:
         SCREEN.blit(BG, (0, 0))
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
-        #BOARD = pygame.image.load("assets/board.png")
-        #SCREEN.blit(BOARD, (0, 0))
+        BOARD = pygame.image.load("assets/board.png")
+        SCREEN.blit(BOARD, (340,100))
         PLAY_TEXT = get_font(45).render("PLAYER VS CPU", True, "White")
-        PLAY_RECT = PLAY_TEXT.get_rect(center=(400, 100))
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 50))
         SCREEN.blit(PLAY_TEXT, PLAY_RECT)
 
-        PLAY_BACK = Button(image=None, pos=(640, 600), 
-                            text_input="BACK", font=get_font(50), base_color="White", hovering_color="Red")
+        PLAY_BACK = Button(image=None, pos=(175, 625), 
+                            text_input="QUIT GAME", font=get_font(35), base_color="White", hovering_color="Red")
 
         PLAY_BACK.changeColor(PLAY_MOUSE_POS)
         PLAY_BACK.update(SCREEN)
@@ -239,7 +238,7 @@ def pvc(dif):
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
-                    difficulty(PVC)
+                    main_menu()
 
         pygame.display.update()
 
@@ -247,14 +246,14 @@ def cvc(dif):
     while True:
         SCREEN.blit(BG, (0, 0))
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
-        #BOARD = pygame.image.load("assets/board.png")
-        #SCREEN.blit(BOARD, (0, 0))
+        BOARD = pygame.image.load("assets/board.png")
+        SCREEN.blit(BOARD, (340,100))
         PLAY_TEXT = get_font(45).render("CPU VS CPU", True, "White")
-        PLAY_RECT = PLAY_TEXT.get_rect(center=(400, 100))
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 50))
         SCREEN.blit(PLAY_TEXT, PLAY_RECT)
 
-        PLAY_BACK = Button(image=None, pos=(640, 600), 
-                            text_input="BACK", font=get_font(50), base_color="White", hovering_color="Red")
+        PLAY_BACK = Button(image=None, pos=(175, 625), 
+                            text_input="QUIT GAME", font=get_font(35), base_color="White", hovering_color="Red")
 
         PLAY_BACK.changeColor(PLAY_MOUSE_POS)
         PLAY_BACK.update(SCREEN)
@@ -265,7 +264,7 @@ def cvc(dif):
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
-                    difficulty(CVC)
+                    main_menu()
 
         pygame.display.update()
 
