239bf1addaab47e1d7605ceafb0268fd034bfd42
luiscunha98 <up201709375@fe.up.pt>
2023-03-10 12:12:48 +0000

    some progress

--- projeto 1/elements/game/dificulty/dificulty.py
+++ projeto 1/elements/game/dificulty/dificulty.py
@@ -2,6 +2,7 @@ from common.functions import *
 from common.classes.button import *
 import sys
 
+
 def difficulty(main_menu, play, mode):
     while True:
         SCREEN.blit(BG, (0, 0))
@@ -59,6 +60,9 @@ def difficulty(main_menu, play, mode):
 
 
 def pvp(main_menu):
+    player_turn = 1  # Player 1 goes first
+    black_pieces = []
+    white_pieces = []
     while True:
         SCREEN.blit(BG, (0, 0))
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
@@ -67,6 +71,13 @@ def pvp(main_menu):
         PLAY_TEXT = get_font(35).render("PLAYER VS PLAYER", True, "White")
         PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 50))
         SCREEN.blit(PLAY_TEXT, PLAY_RECT)
+
+        for POSITION in POSITIONS:
+            pygame.draw.circle(SCREEN, BLUE, POSITION, PIECE_RADIUS)
+
+        # Draw the pieces on the board
+        for piece in black_pieces + white_pieces:
+            pygame.draw.circle(SCREEN, piece['color'], piece['position'], PIECE_RADIUS)
 
         PLAY_BACK = Button(image=None, pos=(175, 625),
                            text_input="QUIT GAME", font=get_font(35), base_color="White", hovering_color="Red")
@@ -81,8 +92,24 @@ def pvp(main_menu):
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
                     main_menu()
+                else:
+                    # Check if the click is within one of the positions
+                    for i, pos in enumerate(POSITIONS):
+                        if distance(pos, PLAY_MOUSE_POS) < PIECE_RADIUS:
+                            # Check if the position is already occupied
+                            if not any(piece['position'] == pos for piece in black_pieces + white_pieces):
+                                # Add the piece to the list
+                                if player_turn == 1 and len(black_pieces) < 4:
+                                    color = BLACK
+                                    black_pieces.append({'position': pos, 'color': color})
+                                    player_turn = 2
+                                elif player_turn == 2 and len(white_pieces) < 4:
+                                    color = WHITE
+                                    white_pieces.append({'position': pos, 'color': color})
+                                    player_turn = 1
 
         pygame.display.update()
+
 
 
 def pvc(main_menu, dif):
